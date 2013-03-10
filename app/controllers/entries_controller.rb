41db04e3fa392bf6b0e8811b0189ddedabb0db4f
dankalmar <dankalmar@gmail.com>
2013-03-10 16:09:54 -0400

    index displays all dids, can create new dids

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,40 @@
 class EntriesController < ApplicationController
 	def index
+		@entry = Entry.all
+	end
+
+	def new
+		@entry = Entry.new
+	end
+
+	def create
+		@entry = Entry.new params[:entry]
+
+		if @entry.save
+			redirect_to entries_path
+		else
+			render :new
+		end	
+	end
+
+	def edit
+	@entry = Entry.find(params[:id])
+	end
+
+	def update
+		@entry = Entry.find(params[:id])
+		if entry.update_attributes params[:entry]
+			redirect_to entries_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@entry = Entry.find(params[:id])
+
+		@entry.destroy
+
+		redirect_to entries_path
 	end
 end
